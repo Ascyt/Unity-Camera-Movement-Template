72f41a591b248c2776d4eada6f0330f156e70885
Ascyt <alarmstufe.cringe@gmail.com>
2023-05-20 21:05:34 +0200

    Added ability to change the rotation target in-game, added tooltips and more

--- Assets/Camera/CameraMovement.cs
+++ Assets/Camera/CameraMovement.cs
@@ -1,22 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    void Update()
+    private void Update()
     {
         MoveControls();
         RotationControls();
     }
+    private void Start()
+    {
+        distanceFromTarget = defaultDistanceFromTarget;
+    }
 
     [SerializeField]
+    [Tooltip("Speed at which the camera moves left, right, upwards, downwards, backwards and forwards")]
     private float speed;
     [SerializeField]
+    [Tooltip("Speed at which the camera moves forward and backward when scrolling")]
     private float scrollSpeed;
     private void MoveControls()
     {
-        Vector3 dir = new Vector3((Input.GetAxis("Horizontal") * speed) + (Input.mouseScrollDelta.x * scrollSpeed), 0f, (Input.GetAxis("Vertical") * speed) + (Input.mouseScrollDelta.y * scrollSpeed));
+        Vector2 scrollDelta = Input.GetMouseButton(1) ? Vector2.zero : Input.mouseScrollDelta;
+
+        Vector3 dir = new Vector3((Input.GetAxis("Horizontal") * speed) + (scrollDelta.x * scrollSpeed), 0f, (Input.GetAxis("Vertical") * speed) + (scrollDelta.y * scrollSpeed));
         if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftControl)) dir.y = -speed;
         if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.LeftShift)) dir.y = speed;
 
@@ -26,39 +35,81 @@ public class CameraMovement : MonoBehaviour
 
     [Space]
     [SerializeField]
+    [Tooltip("Sensitivity for left-clicked first-person rotation")]
     private float rotationSensitivity;
     [SerializeField]
+    [Tooltip("Sensitivity for right-clicked rotation around a point")]
     private float rotateAroundSensitivity;
-    [SerializeField]
-    private float panSensitivity;
 
-    private MousePosition.Point mouseOrigin;
+    private MousePosition.Point? mouseOrigin = null;
 
+    [Space]
     [SerializeField]
+    [Tooltip("Target point to show and rotate around while right-clicking")]
     private Transform target;
     [SerializeField]
+    [Tooltip("Default distance between the camera and the target point")]
+    private float defaultDistanceFromTarget;
     private float distanceFromTarget;
+    [SerializeField]
+    [Tooltip("Sensitivity for scrolling to change the distance between the camera and the target point")]
+    private float distanceFromTargetScrollSensitivity;
+    [SerializeField]
+    [Tooltip("Minimum and maximum distance between the camera and the target point")]
+    private Vector2 distanceFromTargetBounds;
+
+    [Space]
+    [SerializeField]
+    [Tooltip("Sensitivity for the middle-clicked panning of the camera")]
+    private float panSensitivity;
     private void RotationControls()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+        bool isRotating = Input.GetMouseButton(0);
+        bool isRotatingAround = Input.GetMouseButton(1);
+        bool isPanning = Input.GetMouseButton(2);
+        int boolCount = Convert.ToByte(isRotating) + Convert.ToByte(isRotatingAround) + Convert.ToByte(isPanning);
+
+        // Only one of the three can be true at a time
+        if (boolCount == 1) // lock the cursor
         {
-            mouseOrigin = MousePosition.GetCursorPosition();
-            Cursor.lockState = CursorLockMode.Locked;
+            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+            {
+                mouseOrigin = MousePosition.GetCursorPosition();
+                Cursor.lockState = CursorLockMode.Locked;
+            }
         }
-        else if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1) || Input.GetMouseButtonUp(2))
+        else if (boolCount == 0 && mouseOrigin != null) // unlock the cursor (null check is to make sure it only happens once)
         {
             Cursor.lockState = CursorLockMode.None;
-            MousePosition.SetCursorPosition(mouseOrigin);
+            MousePosition.SetCursorPosition(mouseOrigin ?? throw new System.Exception("Achievement earned: How did we get here?"));
+
+            mouseOrigin = null;
+            return;
         }
 
         Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
-        bool isRotating = Input.GetMouseButton(0);
-        bool isRotatingAround = Input.GetMouseButton(1);
-        bool isPanning = Input.GetMouseButton(2);
+        target.gameObject.SetActive(isRotatingAround);
+        // Rotate the camera around the target point
+        if (isRotatingAround)
+        {
+            SetDistanceFromTarget();
+
+            Vector3 pos = Camera.main.ScreenToViewportPoint(mouseDelta);
+
+            // Apply the rotation
+            transform.RotateAround(target.position, Vector3.up, pos.x * rotateAroundSensitivity);
+            transform.RotateAround(target.position, transform.right, -pos.y * rotateAroundSensitivity);
+
+            // Move the target so it stays visually at the center of the camera
+            target.position = transform.position + transform.forward * distanceFromTarget;
+        }
+
+        if (mouseDelta == Vector2.zero) // performance
+            return;
 
         // Rotate camera along Y axis
-        if (isRotating)
+        else if (isRotating)
         {
             Vector3 pos = Camera.main.ScreenToViewportPoint(mouseDelta);
 
@@ -66,21 +117,8 @@ public class CameraMovement : MonoBehaviour
             transform.RotateAround(transform.position, Vector3.up, pos.x * rotationSensitivity);
         }
 
-        // Rotate the camera around the target point
-        target.gameObject.SetActive(isRotatingAround);
-        if (isRotatingAround)
-        {
-            Vector3 pos = Camera.main.ScreenToViewportPoint(mouseDelta);
-
-            // Apply the rotation
-            transform.RotateAround(target.position, Vector3.up, pos.x * rotateAroundSensitivity);
-            transform.RotateAround(target.position, transform.right, -pos.y * rotateAroundSensitivity);
-
-            target.position = transform.position + transform.forward * distanceFromTarget;
-        }
-
         // Move the camera on its XZ plane
-        if (isPanning)
+        else if (isPanning)
         {
             Vector3 pos = Camera.main.ScreenToViewportPoint(mouseDelta);
 
@@ -88,5 +126,25 @@ public class CameraMovement : MonoBehaviour
             move = transform.TransformDirection(move);
             transform.Translate(move, Space.World);
         }
+    }
+
+    // Changes the distance from the target point if scrolled and reverts it to default if left-clicked
+    private void SetDistanceFromTarget()
+    {
+        // When left-clicked, return to default
+        if (Input.GetMouseButtonDown(0))
+        {
+            distanceFromTarget = defaultDistanceFromTarget;
+            return;
+        }
+
+        float scrollDelta = -Input.mouseScrollDelta.y * distanceFromTargetScrollSensitivity;
+
+        if (scrollDelta > 0)
+            distanceFromTarget *= scrollDelta + 1;
+        else
+            distanceFromTarget /= -scrollDelta + 1;
+
+        distanceFromTarget = Mathf.Clamp(distanceFromTarget, distanceFromTargetBounds.x, distanceFromTargetBounds.y);
     }
 }
